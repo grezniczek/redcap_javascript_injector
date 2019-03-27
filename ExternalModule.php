f29f1cc11937fa04a4a3aef9f79fcb304d233e9f
G. Rezniczek <g.rezniczek@gmail.com>
2019-03-27 13:12:47 +0100

    Add code.

--- ExternalModule.php
+++ ExternalModule.php
@@ -1,3 +1,101 @@
 <?php
 
+namespace RUB\JSInjector\ExternalModule;
 
+use ExternalModules\AbstractExternalModule;
+use ExternalModules\ExternalModules;
+
+/**
+ * ExternalModule class for Javascript Injector.
+ */
+class ExternalModule extends AbstractExternalModule {
+
+    function redcap_data_entry_form_top($project_id, $record = null, $instrument, $event_id, $group_id = null, $repeat_instance = 1) {
+        $this->injectJS('data_entry', $instrument);
+    }
+
+    function redcap_survey_page_top($project_id, $record = null, $instrument, $event_id, $group_id = null, $survey_hash, $response_id = null, $repeat_instance = 1) {
+        $this->injectJS('survey', $instrument);
+    }
+
+    /**
+     * Inject JS code.
+     *
+     * @param string $type
+     *   Accepted types: 'data_entry' or 'survey'.
+     * @param string $instrument
+     *   The instrument name.
+     */
+    function injectJS($type, $instrument) {
+        $settings = $this->getFormattedSettings(PROJECT_ID);
+
+        if (empty($settings['js'])) {
+            return;
+        }
+
+        foreach ($settings['js'] as $row) {
+            if (!empty($row['js_enabled']) && in_array($row['js_type'], ['all', $type]) && (!array_filter($row['js_instruments']) || in_array($instrument, $row['js_instruments']))) {
+                echo '<script>' . strip_tags($row['js_code']) . '</script>';
+            }
+        }
+    }
+
+    /**
+     * Formats settings into a hierarchical key-value pair array.
+     *
+     * @param int $project_id
+     *   Enter a project ID to get project settings.
+     *   Leave blank to get system settings.
+     *
+     * @return array
+     *   The formatted settings.
+     */
+    function getFormattedSettings($project_id = null) {
+        $settings = $this->getConfig();
+
+        if ($project_id) {
+            $settings = $settings['project-settings'];
+            $values = ExternalModules::getProjectSettingsAsArray($this->PREFIX, $project_id);
+        }
+        else {
+            $settings = $settings['system-settings'];
+            $values = ExternalModules::getSystemSettingsAsArray($this->PREFIX);
+        }
+
+        return $this->_getFormattedSettings($settings, $values);
+    }
+
+    /**
+     * Auxiliary function for getFormattedSettings().
+     */
+    protected function _getFormattedSettings($settings, $values, $inherited_deltas = []) {
+        $formatted = [];
+
+        foreach ($settings as $setting) {
+            $key = $setting['key'];
+            $value = $values[$key]['value'];
+
+            foreach ($inherited_deltas as $delta) {
+                $value = $value[$delta];
+            }
+
+            if ($setting['type'] == 'sub_settings') {
+                $deltas = array_keys($value);
+                $value = [];
+
+                foreach ($deltas as $delta) {
+                    $sub_deltas = array_merge($inherited_deltas, [$delta]);
+                    $value[$delta] = $this->_getFormattedSettings($setting['sub_settings'], $values, $sub_deltas);
+                }
+
+                if (empty($setting['repeatable'])) {
+                    $value = $value[0];
+                }
+            }
+
+            $formatted[$key] = $value;
+        }
+
+        return $formatted;
+    }
+}
